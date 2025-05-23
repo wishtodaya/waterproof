278c4f3aa99e0f307e724bc11c5312d3f8a28efc
wishtodaya <wishtodaya@gmail.com>
2025-05-23 15:59:26 +0800

    修改我们的服务样式

--- src/components/ServiceSection/index.tsx
+++ src/components/ServiceSection/index.tsx
@@ -1,37 +1,32 @@
-import { View, Text, Swiper, SwiperItem } from '@tarojs/components'
+import { View, Text } from '@tarojs/components'
 import { Service } from 'src/services/api/index/types'
 import './index.scss'
 
 interface ServiceSectionProps {
-  services: Service[]
+  services: Service[];
+  title?: string;
 }
 
-export default function ServiceSection({ services }: ServiceSectionProps) {
+const ServiceSection: React.FC<ServiceSectionProps> = ({ services, title = "我们的服务" }) => {
+  if (!services || services.length === 0) {
+    return null;
+  }
+
   return (
-    <View className='service-section card'>
-      <View className='service-section__header'>
-        <Text className='service-section__title'>我们的服务</Text>
-        <Text className='service-section__subtitle'>专业团队，优质服务</Text>
+    <View className='service-section-container card'>
+      <View className='section-header'>
+        <Text className='section-title'>{title}</Text>
       </View>
-      
-      <Swiper
-        className='service-section__swiper'
-        indicatorColor='#ddd'
-        indicatorActiveColor='#2563EB'
-        circular
-        indicatorDots
-        autoplay
-        interval={5000} // 增加间隔时间，让用户有更多时间阅读
-      >
+      <View className='service-grid'>
         {services.map(service => (
-          <SwiperItem key={service.id}>
-            <View className='service-item'>
-              <Text className='service-item__title'>{service.title}</Text>
-              <Text className='service-item__description'>{service.description}</Text>
-            </View>
-          </SwiperItem>
+          <View key={service.id} className='service-item'>
+            <Text className='service-item-title text-bold'>{service.title}</Text>
+            <Text className='service-item-description'>{service.description}</Text>
+          </View>
         ))}
-      </Swiper>
+      </View>
     </View>
-  )
+  );
 }
+
+export default ServiceSection;
